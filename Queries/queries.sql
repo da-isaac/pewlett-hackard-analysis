185ee679d375d007457b2fcadd6161aa6c69720d
da-isaac <ibjorgen97@gmail.com>
2022-06-15 20:48:06 -0600

    Added retirement info

---
+++ Queries/queries.sql
@@ -0,0 +1,21 @@
+-- Determine retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+-- Count workers who are eligible for retirement
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+-- Insert query into a new table (for export later)
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+-- Validate new table
+SELECT * FROM retirement_info
